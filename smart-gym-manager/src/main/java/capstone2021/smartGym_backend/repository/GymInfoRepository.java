91424b9bb5e01e15d8df9ff3b04268c35d93a8ec
Hyerin <9993hye@naver.com>
2021-11-02 13:30:31 +0900

    init

---
+++ smart-gym-manager/src/main/java/capstone2021/smartGym_backend/repository/GymInfoRepository.java
@@ -0,0 +1,8 @@
+package capstone2021.smartGym_backend.repository;
+
+import capstone2021.smartGym_backend.domain.GymInfo;
+
+public interface GymInfoRepository {
+    boolean update(GymInfo gymInfo);
+    GymInfo read();
+}
